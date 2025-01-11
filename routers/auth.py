958ddc32c590b21b55634ee0df3f93c2157c2d1c
pelinaybar <aybarpelin00@gmail.com>
2025-01-11 13:39:09 +0300

    dockerized

--- routers/auth.py
+++ routers/auth.py
@@ -7,8 +7,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from typing import Annotated
 from sqlalchemy.orm import Session
-from database import SessionLocal
-from models import User
+from ..database import SessionLocal
+from ..models import User
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
